eab6d2e749c932b6ff80251ba74cdbdb1b23b83f
ArcticLampyrid <ArcticLampyrid@outlook.com>
2025-10-05 07:36:19 +0800

    feat: use native titlebar for fewer bugs

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,9 +1,9 @@
 #![windows_subsystem = "windows"]
 
 use clap::Parser;
-use eframe::egui::{self, vec2, Align, ViewportCommand};
-use egui::{Align2, FontId, Id, PointerButton, Sense, UiBuilder};
-use egui::{Button, RichText};
+use eframe::egui::{self, vec2, Align, Style, ViewportCommand, Visuals};
+use egui::RichText;
+use egui::{Align2, UiBuilder};
 use ipp_sharing_core::config::read_config;
 use ipp_sharing_core::ipp_sharing;
 use log::{error, info};
@@ -74,9 +74,9 @@ fn main() {
     let icon = eframe::icon_data::from_png_bytes(include_bytes!("../../icons/app.png")).ok();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_decorations(false)
+            .with_decorations(true)
             .with_maximized(true)
-            .with_transparent(true)
+            .with_close_button(false)
             .with_active(true)
             .with_icon(icon.unwrap_or_default()),
 
@@ -86,6 +86,11 @@ fn main() {
         "IPP Sharing",
         options,
         Box::new(|cc| {
+            let style = Style {
+                visuals: Visuals::dark(),
+                ..Style::default()
+            };
+            cc.egui_ctx.set_style(style);
             setup_fonts(&cc.egui_ctx);
             Ok(Box::new(MyApp::default()))
         }),
@@ -124,18 +129,43 @@ struct MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            title_bar_ui(ui, "IPP Sharing", &mut self.close_confirm_open);
-            ui.horizontal(|ui| {
-                ui.spacing_mut().item_spacing.x = 0.0;
-                ui.label("License: AGPLv3");
-                ui.label(" | ");
-                ui.hyperlink_to(
-                    "Source Code",
-                    "https://github.com/ArcticLampyrid/ipp-sharing",
-                );
-                ui.label(" | ");
-                ui.hyperlink_to("Sponsor", "https://afdian.com/a/alampy");
-            });
+            let app_rect = ui.max_rect();
+            let heading_rect = {
+                let mut rect = app_rect;
+                rect.max.y = rect.min.y + 28.0;
+                rect
+            };
+            ui.scope_builder(
+                UiBuilder::new()
+                    .max_rect(heading_rect)
+                    .layout(egui::Layout::left_to_right(egui::Align::Max)),
+                |ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.heading(format!("IPP Sharing {}", env!("CARGO_PKG_VERSION")));
+                    ui.add_space(8.0);
+
+                    ui.label("License: AGPLv3");
+                    ui.label(" | ");
+                    ui.hyperlink_to(
+                        "Source Code",
+                        "https://github.com/ArcticLampyrid/ipp-sharing",
+                    );
+                    ui.label(" | ");
+                    ui.hyperlink_to("Sponsor", "https://afdian.com/a/alampy");
+                },
+            );
+            ui.scope_builder(
+                UiBuilder::new()
+                    .max_rect(heading_rect)
+                    .layout(egui::Layout::right_to_left(egui::Align::Max)),
+                |ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+
+                    if ui.button(RichText::new("Exit").size(20.0)).clicked() {
+                        self.close_confirm_open = true;
+                    }
+                },
+            );
             egui::Window::new("Close Confirmation")
                 .open(&mut self.close_confirm_open)
                 .collapsible(false)
@@ -152,72 +182,4 @@ impl eframe::App for MyApp {
             egui_logger::LoggerUi::default().show(ui);
         });
     }
-}
-
-fn title_bar_ui(ui: &mut egui::Ui, title: &str, close_confirm_open: &mut bool) {
-    let app_rect = ui.max_rect();
-    let title_bar_height = 32.0;
-    let title_bar_rect = {
-        let mut rect = app_rect;
-        rect.max.y = rect.min.y + title_bar_height;
-        rect
-    };
-
-    ui.painter().text(
-        title_bar_rect.center(),
-        Align2::CENTER_CENTER,
-        title,
-        FontId::proportional(20.0),
-        ui.style().visuals.text_color(),
-    );
-
-    let title_bar_response = ui.interact(
-        title_bar_rect,
-        Id::new("title_bar"),
-        Sense::click_and_drag(),
-    );
-
-    if title_bar_response.double_clicked() {
-        let is_maximized = ui.input(|i| i.viewport().maximized.unwrap_or(false));
-        ui.ctx()
-            .send_viewport_cmd(ViewportCommand::Maximized(!is_maximized));
-    }
-
-    if title_bar_response.drag_started_by(PointerButton::Primary) {
-        ui.ctx().send_viewport_cmd(ViewportCommand::StartDrag);
-    }
-
-    ui.scope_builder(
-        UiBuilder::new()
-            .max_rect(title_bar_rect)
-            .layout(egui::Layout::left_to_right(egui::Align::Center)),
-        |ui| {
-            ui.spacing_mut().item_spacing.x = 0.0;
-
-            let button_height = 20.0;
-            let close_response = ui
-                .add(Button::new(RichText::new("Close").size(button_height)))
-                .on_hover_text("Close the window");
-            if close_response.clicked() {
-                *close_confirm_open = true;
-            }
-        },
-    );
-
-    ui.scope_builder(
-        UiBuilder::new()
-            .max_rect(title_bar_rect)
-            .layout(egui::Layout::right_to_left(egui::Align::Center)),
-        |ui| {
-            ui.spacing_mut().item_spacing.x = 0.0;
-
-            let button_height = 20.0;
-            let minimized_response = ui
-                .add(Button::new(RichText::new("Minimize").size(button_height)))
-                .on_hover_text("Minimize the window");
-            if minimized_response.clicked() {
-                ui.ctx().send_viewport_cmd(ViewportCommand::Minimized(true));
-            }
-        },
-    );
 }
